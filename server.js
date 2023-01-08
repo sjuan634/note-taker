74b3cffe85927cbb87770e7100b3c4aa72f5e094
sanchezju <sjuan634@yahoo.com>
2023-01-08 14:07:27 -0800

    set up routes to handel get and post

---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const database = require("./db/db")
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+})
+
+app.route("/api/notes")
+    .get(function (req, res) {
+        res.json(database);
+    })
+    .post(function (req, res) {
+        let jsonFilePath = path.join(__dirname, "/db/db.json");
+        let newNote = req.body;
+
+        newNote.id = Date.now();
+        database.push(newNote)
+
+        fs.writeFile(jsonFilePath, JSON.stringify(database), function (err) {
+
+            if (err) {
+                return console.log(err);
+            }
+            console.log("Your note was saved!");
+        });
+        res.json(newNote);
+    });
+
+app.delete("/api/notes/:id", function (req, res) {
+    let jsonFilePath = path.join(__dirname, "/db/db.json");
+    for (let i = 0; i < database.length; i++) {
+
+        if (database[i].id == req.params.id) {
+            database.splice(i, 1);
+            break;
+        }
+    }
+    fs.writeFileSync(jsonFilePath, JSON.stringify(database), function (err) {
+
+        if (err) {
+            return console.log(err);
+        } else {
+            console.log("Your note was deleted!");
+        }
+    });
+    res.json(database);
+});
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
